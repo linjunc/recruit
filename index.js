635e57d12635d9ce6185324d938db12a776ee655
linjunc <ljc15113180751@163.com>
2021-08-30 12:15:19 +0800

    测试提交

--- index.js
+++ index.js
@@ -2,19 +2,27 @@
  * @Author: 林俊丞
  * @Date: 2021-08-29 09:33:29
  * @LastEditors: 林俊丞
- * @LastEditTime: 2021-08-29 12:30:06
+ * @LastEditTime: 2021-08-30 12:14:49
  * @Description: 
  */
 function mySubmit(form) {
+    // 上传数据
     let myData = []
+    // 获取表单数据
     let formData = new FormData(form)
+    console.log(formData);
     let userData = formData.getAll('username')
+    // 第一第二资源数据
     let recruitData = formData.getAll('week')
     let recruitDataWish = formData.getAll('wish')
+    // 获取body以及提示框
     const showBox = document.querySelector('.showBox')
     const body = document.querySelector('body')
-    myData = [...userData, ...recruitData,...recruitDataWish]
+    // 数据整合
+    myData = [...userData, ...recruitData, ...recruitDataWish]
+    // 弹出提示框
     showBox.style.display = 'block'
+    // 显示报名数据
     showBox.innerHTML = myData
     body.className = 'bgFix'
     return false
